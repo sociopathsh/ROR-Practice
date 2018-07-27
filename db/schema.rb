b486f04bf3d3bf0d97b56c63d953b47c6e0965f2
SoeHtut <soehtut@escapepixel.com>
2018-07-28 01:07:29 +0630

    Modified users

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2018_07_23_045626) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
