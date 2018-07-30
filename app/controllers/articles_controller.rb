a9a9464e6591cdec730e6b5bbb250d6abf8873c5
SoeHtut <soehtut@escapepixel.com>
2018-07-30 16:11:38 +0630

    added association between users and articles. added debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,9 @@ class ArticlesController < ApplicationController
 
   def create
     # render plain: params[:article].inspect
+    # debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = 'New article was successfully created'
       redirect_to article_path(@article)
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_23_045626) do
+ActiveRecord::Schema.define(version: 2018_07_30_091017) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "description"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
