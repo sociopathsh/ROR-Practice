4bc4a73c8f1f09c18e19d439f02d843492f093e2
SoeHtut <soehtut@escapepixel.com>
2018-07-31 12:23:52 +0630

    signup from UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#home'
   get 'about', to: 'welcome#about'
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
 end
